c1604c4e36dbe7b7fa7831049c835271f7244eb4
JoaoBortotti <joao.bortotti@uotz.com.br>
2022-09-07 18:14:45 -0300

    setting nvim theme to hybrid_material

--- nvim/lua/configs/settings.lua
+++ nvim/lua/configs/settings.lua
@@ -33,11 +33,11 @@ vim.cmd([[
 ]])
 
 -- Colorscheme
-vim.cmd([[ set termguicolors ]])
-vim.cmd([[ set bg=dark ]])
+vim.cmd([[ let g:hybrid_transparent_background = 1 ]])
+vim.cmd([[ colorscheme hybrid_material ]])
+-- vim.cmd([[ set bg=dark ]])
 vim.cmd([[ set fillchars=eob:\ ]])
-vim.cmd([[ highlight Pmenu ctermbg=gray guibg=gray]])
 vim.cmd([[ highlight SignColumn guibg=bg ]])
-vim.cmd([[ highlight GitSignsAdd guibg=bg ctermbg=black guifg=green ]])
-vim.cmd([[ highlight GitSignsChange guibg=bg ctermbg=black guifg=yellow ]])
-vim.cmd([[ highlight GitSignsDelete guibg=bg ctermbg=black guifg=red ]])
+-- vim.cmd([[ highlight GitSignsAdd guibg=bg ctermbg=black guifg=green ]])
+-- vim.cmd([[ highlight GitSignsChange guibg=bg ctermbg=black guifg=yellow ]])
+-- vim.cmd([[ highlight GitSignsDelete guibg=bg ctermbg=black guifg=red ]])
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,147 +1,146 @@
 vim.cmd([[packadd packer.nvim]])
-
 return require("packer").startup(function()
-	-- Utils --
+  -- Utils --
 
-	-- Package manager
-	use("wbthomason/packer.nvim")
+  -- Package manager
+  use("wbthomason/packer.nvim")
 
-	use({
-		"windwp/nvim-autopairs",
-		config = function()
-			require("plugins/autopairs").setup()
-		end,
-	})
+  use({
+    "windwp/nvim-autopairs",
+    config = function()
+      require("plugins/autopairs").setup()
+    end,
+  })
 
-	-- Toggleterm
-	use({
-		"akinsho/toggleterm.nvim",
-		config = function()
-			require("plugins/toggleterm").setup()
-		end,
-	})
+  -- Toggleterm
+  use({
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("plugins/toggleterm").setup()
+    end,
+  })
 
-	-- Highlight
-	use("RRethy/vim-illuminate")
+  -- Highlight
+  use("RRethy/vim-illuminate")
 
-	-- Project managment
-	use({
-		"ahmedkhalf/project.nvim",
-		config = function()
-			require("plugins/project").setup()
-		end,
-	})
+  -- Project managment
+  use({
+    "ahmedkhalf/project.nvim",
+    config = function()
+      require("plugins/project").setup()
+    end,
+  })
 
-	-- Comment
-	use({
-		"numToStr/Comment.nvim",
-		config = function()
-			require("plugins/comment").setup()
-		end,
-	})
+  -- Comment
+  use({
+    "numToStr/Comment.nvim",
+    config = function()
+      require("plugins/comment").setup()
+    end,
+  })
 
-	-- Lualine
-	use({
-		"nvim-lualine/lualine.nvim",
-		config = function()
-			require("plugins/lualine").setup()
-		end,
-	})
+  -- Lualine
+  use({
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("plugins/lualine").setup()
+    end,
+  })
 
-	use({
-		"SmiteshP/nvim-gps", -- Lua line LSP infos provider
-		config = function()
-			require("nvim-gps").setup()
-		end,
-	})
+  use({
+    "SmiteshP/nvim-gps", -- Lua line LSP infos provider
+    config = function()
+      require("nvim-gps").setup()
+    end,
+  })
 
-	-- Nvim tree
-	use({
-		"kyazdani42/nvim-tree.lua", -- nvim tree
-		requires = {
-			"kyazdani42/nvim-web-devicons", -- optional, for file icon
-		},
-		tag = "nightly", -- optional, updated every week. (see issue #1193)
-		config = function()
-			require("plugins/nvim-tree").setup()
-		end,
-	})
+  -- Nvim tree
+  use({
+    "kyazdani42/nvim-tree.lua", -- nvim tree
+    requires = {
+      "kyazdani42/nvim-web-devicons", -- optional, for file icon
+    },
+    tag = "nightly", -- optional, updated every week. (see issue #1193)
+    config = function()
+      require("plugins/nvim-tree").setup()
+    end,
+  })
 
-	-- Git
-	use({
-		"lewis6991/gitsigns.nvim", -- gitsigns
-		config = function()
-			require("plugins/gitsigns").setup()
-		end,
-	})
+  -- Git
+  use({
+    "lewis6991/gitsigns.nvim", -- gitsigns
+    config = function()
+      require("plugins/gitsigns").setup()
+    end,
+  })
 
-	-- Telescope
-	use({
-		"nvim-telescope/telescope.nvim", -- Telescope
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+  -- Telescope
+  use({
+    "nvim-telescope/telescope.nvim", -- Telescope
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
 
-	-- LSP
-	use({ -- LSP config provider
-		"neovim/nvim-lspconfig",
-		opt = true,
-		event = "BufReadPre",
-		config = function()
-			require("plugins/lsp").setup()
-		end,
-	})
+  -- LSP
+  use({ -- LSP config provider
+    "neovim/nvim-lspconfig",
+    opt = true,
+    event = "BufReadPre",
+    config = function()
+      require("plugins/lsp").setup()
+    end,
+  })
 
-	use("williamboman/nvim-lsp-installer") -- LSP Server Installer
+  use("williamboman/nvim-lsp-installer") -- LSP Server Installer
 
-	use("jose-elias-alvarez/null-ls.nvim") -- Null-ls
+  use("jose-elias-alvarez/null-ls.nvim") -- Null-ls
 
-	-- CMP packages
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("saadparwaiz1/cmp_luasnip")
-	use("ray-x/lsp_signature.nvim")
-	use("onsails/lspkind-nvim")
+  -- CMP packages
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-cmdline")
+  use("saadparwaiz1/cmp_luasnip")
+  use("ray-x/lsp_signature.nvim")
+  use("onsails/lspkind-nvim")
 
-	-- Snippets
-	use("L3MON4D3/LuaSnip")
-	use("rafamadriz/friendly-snippets")
+  -- Snippets
+  use("L3MON4D3/LuaSnip")
+  use("rafamadriz/friendly-snippets")
 
-	-- Tree sitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("plugins/treesitter").setup()
-		end,
-	})
+  -- Tree sitter
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("plugins/treesitter").setup()
+    end,
+  })
 
-	-- Neorg
-	use({
-		"nvim-neorg/neorg",
-		config = function()
-			require("plugins/neorg").setup()
-		end,
-		requires = "nvim-lua/plenary.nvim",
-	})
+  -- Neorg
+  use({
+    "nvim-neorg/neorg",
+    config = function()
+      require("plugins/neorg").setup()
+    end,
+    requires = "nvim-lua/plenary.nvim",
+  })
 
-	-- Tabline
-	use({
-		"kdheepak/tabline.nvim",
-		config = function()
-			require("plugins/tabline").setup()
-		end,
-	})
+  -- Tabline
+  use({
+    "kdheepak/tabline.nvim",
+    config = function()
+      require("plugins/tabline").setup()
+    end,
+  })
 
-	-- Dashboard
-	use({
-		"glepnir/dashboard-nvim",
-		config = function()
-			require("plugins/dashboard").setup()
-		end,
-	})
+  -- Dashboard
+  use({
+    "glepnir/dashboard-nvim",
+    config = function()
+      require("plugins/dashboard").setup()
+    end,
+  })
 
-	-- Colorschemes
-	use("morhetz/gruvbox")
+  -- Colorschemes
+  use("kristijanhusak/vim-hybrid-material")
 end)
