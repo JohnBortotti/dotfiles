311c977d2794d27e4a8bd623c199699aa338a9e8
JohnBortotti <joao_bortotti@hotmail.com>
2023-12-21 01:17:14 -0300

    add validation message in list-containers.sh

--- scripts/docker/list-containers.sh
+++ scripts/docker/list-containers.sh
@@ -1,6 +1,9 @@
-directory=$1
+if [[ -z "$1" ]]; then
+    echo "Please provide a directory."
+    exit 1
+fi
 
-for file in $(find $directory -name "docker-compose.yml"); do 
+for file in $(find $1 -name "docker-compose.yml"); do 
   echo
   echo "Container status in $file:"
   cd $(dirname $file)
