4f20c6531c161bc4bda983032bc699eec9dd0dfc
JohnBortotti <joao_bortotti@hotmail.com>
2024-03-24 20:09:10 +0000

    update setup script logs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,20 +2,20 @@
 echo "#########################################"
 echo "   Installation script by JohnBortotti   "
 echo "#########################################"
-echo -e "\n"
+echo ""
 
 # check if Nix is already installed
 if ! command -v nix &> /dev/null
 then
     # download and install Nix
-    echo -e "installing Nix...\n"
+    echo -e "installing Nix..."
     sh <(curl -L https://nixos.org/nix/install) --daemon
 else
-    echo -e "Nix is already installed, skipping installation...\n"
+    echo -e "Nix is already installed, skipping installation..."
 fi
 
 # enable experimental features
-echo -e "enabling nix experimental features (commands and flakes)...\n"
+echo -e "enabling nix experimental features (commands and flakes)..."
 mkdir -p ~/.config/nix
 echo -e "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
 
@@ -23,27 +23,27 @@ echo -e "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
 if ! command -v home-manager &> /dev/null
 then
     # download and install Nix Home Manager
-    echo -e "Installing Home Manager...\n"
+    echo -e "Installing Home Manager..."
     nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
     nix-channel --update
     nix-shell '<home-manager>' -A install
 else
-    echo -e "Home Manager is already installed. Skipping installation...\n"
+    echo -e "Home Manager is already installed. Skipping installation..."
 fi
 
 # check if the dotfiles directory already exists
 if [ -d "$HOME/dotfiles" ]; then
-    echo -e "dotfiles directory already exists. Updating repository...\n"
+    echo -e "dotfiles directory already exists. Updating repository..."
     cd ~/dotfiles
     git pull
 else
     # clone dotfiles repository
-    echo -e "cloning dotfiles repository...\n"
+    echo -e "cloning dotfiles repository..."
     git clone https://github.com/JohnBortotti/dotfiles.git ~/dotfiles || { echo "cloning failed. Exiting..."; exit 1; }
 fi
 
 # nun Home Manager flake
-echo -e "running Home Manager flake...\n"
+echo -e "running Home Manager flake..."
 cd ~/dotfiles/nix/home-manager
 nix run .#homeConfigurations.joao.activationPackage
 
@@ -51,14 +51,14 @@ nix run .#homeConfigurations.joao.activationPackage
 echo ""
 read -p "Do you want to install Tailscale? (y/n): " install_tailscale
 if [[ "$install_tailscale" == "y" || "$install_tailscale" == "Y" ]]; then
-    echo -e "Installing Tailscale...\n"
+    echo -e "Installing Tailscale..."
     curl -fsSL https://tailscale.com/install.sh | sh
 else
-    echo -e "Skipping Tailscale installation...\n"
+    echo -e "Skipping Tailscale installation..."
 fi
 
 # replace .bashrc with the one from dotfiles
-echo -e "replacing .bashrc with the one from dotfiles...\n"
+echo -e "replacing .bashrc with the one from dotfiles..."
 cp ~/dotfiles/bash/.bashrc ~/.bashrc
 
-echo -e "\nInstallation complete!\n\n"
+echo -e "Installation complete!\n\n"
